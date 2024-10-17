0ea7bdcd03472e4b5833aa45e5fe7834d03a0c46
Alexander Bennetsen Colmorn <alexander.colmorn@danfoss.com>
2024-10-17 10:16:10 +0200

    Added Domain Project and Entities

---
+++ TicketManager.Management.Domain/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketManager.Management.Domain.Common;
+
+namespace TicketManager.Management.Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        // Navigation properties
+        public List<Event>? Events { get; set; }
+    }
+}
---
+++ TicketManager.Management.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketManager.Management.Domain.Common;
+
+namespace TicketManager.Management.Domain.Entities
+{
+    public class Order : AuditableEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
