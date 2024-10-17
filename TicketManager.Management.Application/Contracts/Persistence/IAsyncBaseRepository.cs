8c229217fa31c53008225119ad02efdea8db9cb3
Alexander Bennetsen Colmorn <alexander.colmorn@danfoss.com>
2024-10-17 10:38:06 +0200

    Added Application layer and contracts for Persistence

---
+++ TicketManager.Management.Application/Contracts/Persistence/IAsyncBaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketManager.Management.Application.Contracts.Persistence
+{
+    public interface IAsyncBaseRepository<T> where T : class
+    {
+        /// <summary>
+        /// Get an entity based on its Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<T> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Get all entites for a given repo
+        /// </summary>
+        /// <returns></returns>
+        Task<IReadOnlyList<T>> ListAllAsync();
+
+        /// <summary>
+        /// Add an entity
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<T> AddAsync(T entity);
+
+        /// <summary>
+        /// Update an entity
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task UpdateAsync(T entity);
+
+        /// <summary>
+        /// Delete an entity
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ TicketManager.Management.Application/Contracts/Persistence/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketManager.Management.Domain.Entities;
+
+namespace TicketManager.Management.Application.Contracts.Persistence
+{
+    public interface ICategoryRepository : IAsyncBaseRepository<Category>
+    {
+    }
+}
