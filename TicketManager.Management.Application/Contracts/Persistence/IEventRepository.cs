1155989a7771acbd4b6ac43a738144b455a920da
Alexander Bennetsen Colmorn <alexander.colmorn@danfoss.com>
2024-10-18 08:40:11 +0200

    Added Create logic for Events

--- TicketManager.Management.Application/Contracts/Persistence/IEventRepository.cs
+++ TicketManager.Management.Application/Contracts/Persistence/IEventRepository.cs
@@ -9,5 +9,12 @@ namespace TicketManager.Management.Application.Contracts.Persistence
 {
     public interface IEventRepository : IAsyncBaseRepository<Event>
     {
+        /// <summary>
+        /// Checks whether an event with the given name and date is unique.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="eventDate"></param>
+        /// <returns>a boolean true if its Unique</returns>
+        Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate);
     }
 }
--- TicketManager.Management.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ TicketManager.Management.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TicketManager.Management.Application.Contracts.Persistence;
+using TicketManager.Management.Application.Exeptions;
 using TicketManager.Management.Domain.Entities;
 
 namespace TicketManager.Management.Application.Features.Events.Commands.CreateEvent
@@ -25,8 +26,19 @@ namespace TicketManager.Management.Application.Features.Events.Commands.CreateEv
         public async Task<int> Handle(CreateEventCommand request, CancellationToken cancellationToken)
         {
             Event mappedEvent = _mapper.Map<Event>(request);
-            
+
+            var validator = new CreateEventCommandValidator(_eventRepository);
+            var validatorResult = await validator.ValidateAsync(request);
+
+
+            if (validatorResult.Errors.Count > 0)
+                throw new ValidationException(validatorResult);
+
+
             mappedEvent = await _eventRepository.AddAsync(mappedEvent);
+
+
+
 
             return mappedEvent.EventId;
         }
