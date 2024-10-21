7c4be9438681f6fb14bb20f6b21331eb4dd5b8c3
Alexander Bennetsen Colmorn <alexander.colmorn@danfoss.com>
2024-10-21 12:37:45 +0200

    Added Event and Order Controller

---
+++ TicketManager.Management.Api/Controllers/EventController.cs
@@ -0,0 +1,78 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TicketManager.Management.Application.Features.Events.Commands.CreateEvent;
+using TicketManager.Management.Application.Features.Events.Commands.DeleteEvent;
+using TicketManager.Management.Application.Features.Events.Commands.UpdateEvent;
+using TicketManager.Management.Application.Features.Events.Queries.GetEventDetail;
+using TicketManager.Management.Application.Features.Events.Queries.GetEventsList;
+
+namespace TicketManager.Management.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public EventController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpGet(Name = "GetAllEvents")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<List<EventListVm>>> GetAllEvents()
+        {
+            var result = await _mediator.Send(new GetEventsListQuery());
+            return Ok(result);
+        }
+
+
+        [HttpGet("{id}", Name = "GetEventById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<List<EventListVm>>> GetEventById(int id)
+        {
+            var result = await _mediator.Send(new GetEventDetailQuery() { Id = id });
+            return Ok(result);
+        }
+
+        [HttpPost(Name = "AddEvent")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<ActionResult<int>> Create([FromBody] CreateEventCommand createEventCommand)
+        {
+            var id = await _mediator.Send(createEventCommand);
+            return CreatedAtRoute("GetEventById", new { id }, id);
+        }
+
+
+        [HttpPut(Name = "UpdateEvent")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Update([FromBody] UpdateEventCommand updateEventCommand)
+        {
+            await _mediator.Send(updateEventCommand);
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}", Name = "DeleteEvent")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var delete = new DeleteEventCommand() { EventId = id };
+            await _mediator.Send(delete);
+            return NoContent();
+        }
+
+    }
+        
+    
+    
+}
---
+++ TicketManager.Management.Api/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketManager.Management.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+    }
+}
