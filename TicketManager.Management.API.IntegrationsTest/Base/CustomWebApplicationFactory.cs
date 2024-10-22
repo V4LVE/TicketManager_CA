e5fe5d97ad1b40b984a5fe717e91851fe9ddf4e2
Alexander Bennetsen Colmorn <alexander.colmorn@danfoss.com>
2024-10-22 09:39:16 +0200

    Added Basic integration testing for the category api controller endpoint

---
+++ TicketManager.Management.API.IntegrationsTest/Base/CustomWebApplicationFactory.cs
@@ -0,0 +1,57 @@
+﻿using Castle.Core.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketManager.Management.Persistence;
+
+namespace TicketManager.Management.API.IntegrationsTest.Base
+{
+    public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddDbContext<TicketManagerDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TickerManagerInMemoryTest");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopredServices = scope.ServiceProvider;
+
+                    var context = scopredServices.GetRequiredService<TicketManagerDbContext>();
+
+                    var logger = scopredServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TProgram>>>();
+
+                    context.Database.EnsureCreated();
+
+                    try
+                    {
+                        Utilities.InitializeDbForTests(context);
+                    }
+                    catch (Exception ex)
+                    {
+
+                        logger.LogError(ex, $"An error occurred seeding the database with test messages. Error: {ex.Message}");
+                    }
+                }
+            });
+        }
+
+        public HttpClient GetAnonClient()
+        {
+            return CreateClient();
+        }
+    }
+}
---
+++ TicketManager.Management.API.IntegrationsTest/Base/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketManager.Management.Application.Contracts.Persistence;
+using TicketManager.Management.Domain.Entities;
+using TicketManager.Management.Persistence;
+
+namespace TicketManager.Management.API.IntegrationsTest.Base
+{
+    public class Utilities
+    {
+        public static void InitializeDbForTests(TicketManagerDbContext context)
+        {
+            int concert = 1;
+            int musical = 2;
+            int sports = 3;
+            int conference = 4;
+
+            context.Categories.Add(new Domain.Entities.Category { CategoryId = concert, Name = "Concert" });
+            context.Categories.Add(new Domain.Entities.Category { CategoryId = musical, Name = "Musical" });
+            context.Categories.Add(new Domain.Entities.Category { CategoryId = sports, Name = "Sports" });
+            context.Categories.Add(new Domain.Entities.Category { CategoryId = conference, Name = "Conference" });
+
+            context.SaveChanges();
+        }
+
+       
+    }
+}
